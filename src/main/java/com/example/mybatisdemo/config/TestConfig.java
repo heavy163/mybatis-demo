740855eca3226f1841d98db3577e0be26232a16c
zhanhf <zhanhaifei@126.com>
2020-07-29 16:03:19 +0800

    修改mapper位置
    Change-Id: Ib77fef095a8a5ee908187422c3f830caeb1f75cc

--- src/main/java/com/example/mybatisdemo/config/TestConfig.java
+++ src/main/java/com/example/mybatisdemo/config/TestConfig.java
@@ -60,7 +60,7 @@ public class TestConfig {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource());
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:com/gitee/sunchenbin/mybatis/actable/mapping/*/*.xml"));
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:mapper/*.xml"));
         sqlSessionFactoryBean.setTypeAliasesPackage("com.example.entity.*");
         return sqlSessionFactoryBean;
     }
